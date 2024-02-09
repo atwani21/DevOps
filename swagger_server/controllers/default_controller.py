6eb6cfb07af5ebed164161d7c576cccb251d528c
Priyanka <priyankaatwani@live.com>
2024-02-09 15:00:05 +0100

    eddited add method

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -17,7 +17,8 @@ def add_student(body=None):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return add(body)
+        return add(body)
+    return 500, 'error'
 
 
 
